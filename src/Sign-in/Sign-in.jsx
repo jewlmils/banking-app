e18e5822b01629995d159efb2abf79b122eddc64
jewlmils <st.jewelmilagrosa@gmail.com>
2023-10-27 19:12:54 +0800

    budget app is working, still need to work on error handling and balance

--- src/Sign-in/Sign-in.jsx
+++ src/Sign-in/Sign-in.jsx
@@ -1,6 +1,6 @@
 import React, { useRef, useState } from "react";
 import "../Sign-in/Sign-in.css";
-import Content from "../Components/Content";
+import Content from "../Budget/Content";
 
 export default function Signin() {
   const emailRef = useRef();
