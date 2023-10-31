f4323d4199924286b2898fd3fa373ba86b6f0e07
jujyne <egmisa.am@gmail.com>
2023-10-31 12:23:52 +0800

    added private routes but need to add the login

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 
 import "./App.css";
+import { Login } from "./pages/Login.jsx";
 import { Sidebar } from "./components/Sidebar.jsx";
 import { Header } from "./components/Header.jsx";
 import { Overview } from "./pages/Overview.jsx";
@@ -14,43 +15,55 @@ import { Budget } from "./pages/Budget.jsx";
 import { BuyLoad } from "./pages/BuyLoad.jsx";
 import { Goals } from "./pages/Goals.jsx";
 import { Route, Routes } from "react-router-dom";
+import { PrivateRoutes } from "./utils/PrivateRoutes";
 
 export const userRole = "user";
+export const login  = true;
+
 const adminRouter = (
   <Routes>
-    <Route path="/" element={<Overview />} />
-    <Route path="/create-new-user" element={<CreateUser />} />
-    <Route path="/accounts" element={<Accounts />} />
-    <Route path="/deposit" element={<Deposit />} />
-    <Route path="/withdraw" element={<Withdraw />} />
-    <Route path="/send-money" element={<SendMoney />} />
-    <Route path="/currency" element={<Currency />} />
+    <Route>
+      <Route path="/" element={<Overview />} />
+      <Route path="/create-new-user" element={<CreateUser />} />
+      <Route path="/accounts" element={<Accounts />} />
+      <Route path="/deposit" element={<Deposit />} />
+      <Route path="/withdraw" element={<Withdraw />} />
+      <Route path="/send-money" element={<SendMoney />} />
+      <Route path="/currency" element={<Currency />} />
+    </Route>
   </Routes>
 );
 const customerRouter = (
   <Routes>
-    <Route path="/" element={<Overview />} />
-    <Route path="/send-money" element={<SendMoney />} />
-    <Route path="/buy-load" element={<BuyLoad />} />
-    <Route path="/budget" element={<Budget />} />
-    <Route path="/goals" element={<Goals />} />
-    <Route path="/currency" element={<Currency />} />
+    <Route>
+      <Route path="/" element={<Overview />} />
+      <Route path="/send-money" element={<SendMoney />} />
+      <Route path="/buy-load" element={<BuyLoad />} />
+      <Route path="/budget" element={<Budget />} />
+      <Route path="/goals" element={<Goals />} />
+      <Route path="/currency" element={<Currency />} />
+    </Route>
   </Routes>
 );
 
-function App() {
-  const routes = userRole === "admin" ? adminRouter : customerRouter;
 
+function Homepage(){
+  const routes = userRole === "admin" ? adminRouter : customerRouter;
+  return(<div className="body">
+<Sidebar />
+<main>
+  <Header />
+  <section className="content">
+    <div className="content-container">{routes}</div>
+  </section>
+</main>
+</div>)
+} 
+
+function App() {
+  const pageDisplay = login ?  <Homepage/>: <Login/>;
   return (
-    <div className="body">
-      <Sidebar />
-      <main>
-        <Header />
-        <section className="content">
-          <div className="content-container">{routes}</div>
-        </section>
-      </main>
-    </div>
+    pageDisplay
   );
 }
 
