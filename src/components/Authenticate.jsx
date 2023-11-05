6a394c37d32ec2d20880a106b090c851cb914c57
jujyne <egmisa.am@gmail.com>
2023-11-05 17:16:32 +0800

    fix logout button

--- src/components/Authenticate.jsx
+++ src/components/Authenticate.jsx
@@ -4,14 +4,11 @@ import { Login } from "./Login";
 import { Dashboard, adminRouter, customerRouter } from "./Dashboard";
 import "../style/budget.css";
 
-
-
 export function Authenticate() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [isAdmin, setIsAdmin] = useState(false);
   const [client, setClient] = useState(null);
   const [error, setError] = useState("");
-  
 
   const isLoginSuccess = (email, password) => {
     const user = userData.find(
@@ -29,35 +26,42 @@ export function Authenticate() {
   };
 
   const logout = () => {
+    console.log("Logout function called");
     setIsLoggedIn(false);
     setIsAdmin(false);
     setClient(null);
     setError("");
-    localStorage.removeItem('currentUser');
+    localStorage.removeItem("currentUser");
+    console.log("Logout completed");
   };
 
-useEffect(()=>{
-  const currentUser = JSON.parse(localStorage.getItem("currentUser"));
-  if(currentUser){
-    setIsLoggedIn(true)
-    setClient(currentUser)
-  }
-
-}, [])
-
-
+  useEffect(() => {
+    const currentUser = JSON.parse(localStorage.getItem("currentUser"));
+    if (currentUser) {
+      setIsLoggedIn(true);
+      setClient(currentUser);
+    }
+  }, []);
 
   if (isLoggedIn) {
-    localStorage.setItem('currentUser', JSON.stringify(client));
-    
+    localStorage.setItem("currentUser", JSON.stringify(client));
+
     if (isAdmin) {
-      
-      return <Dashboard user={client} handleLogout={logout} routes={adminRouter}/>;
+      return (
+        <>
+          <Dashboard user={client} routes={adminRouter} />;
+          <button onClick={logout}>Logout</button>
+        </>
+      );
     } else {
-      return <Dashboard routes={customerRouter}/>
+      return (
+        <>
+          <Dashboard routes={customerRouter} />
+          <button onClick={logout}>Logout</button>
+        </>
+      );
     }
   } else {
-    return (<Login loginHandler={isLoginSuccess} error={error} />
-    )
+    return <Login loginHandler={isLoginSuccess} error={error} />;
   }
 }
