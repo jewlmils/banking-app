2f59d62c45e11bd807e128a017065f41edc9e862
Erollercoaster <erollfern@gmail.com>
2023-11-07 12:08:27 +0800

    fix the local storage logic

--- src/pages/accounts/Accounts.jsx
+++ src/pages/accounts/Accounts.jsx
@@ -60,124 +60,125 @@ export function Accounts() {
   };
 
   return (
- 
-      <div className="wrapper">
-        
-        <div className="searchBar">
-          <input
-            id="searchQueryInput"
-            type="text"
-            name="searchQueryInput"
-            placeholder="Search"
-            value={searchQuery}
-            onChange={handleSearchChange}
-          />
-          <button id="searchQuerySubmit" type="submit" name="searchQuerySubmit">
-            <svg style={{ width: "24px", height: "24px" }} viewBox="0 0 24 24">
-              <path
-                fill="#666666"
-                d="M9.5,3A6.5,6.5 0 0,1 16,9.5C16,11.11 15.41,12.59 14.44,13.73L14.71,14H15.5L20.5,19L19,20.5L14,15.5V14.71L13.73,14.44C12.59,15.41 11.11,16 9.5,16A6.5,6.5 0 0,1 3,9.5A6.5,6.5 0 0,1 9.5,3M9.5,5C7,5 5,7 5,9.5C5,12 7,14 9.5,14C12,14 14,12 14,9.5C14,7 12,5 9.5,5Z"
-              />
-            </svg>
-          </button>
-        </div>
+    <div className="wrapper">
+      <div className="searchBar">
+        <input
+          id="searchQueryInput"
+          type="text"
+          name="searchQueryInput"
+          placeholder="Search"
+          value={searchQuery}
+          onChange={handleSearchChange}
+        />
+        <button id="searchQuerySubmit" type="submit" name="searchQuerySubmit">
+          <svg style={{ width: "24px", height: "24px" }} viewBox="0 0 24 24">
+            <path
+              fill="#666666"
+              d="M9.5,3A6.5,6.5 0 0,1 16,9.5C16,11.11 15.41,12.59 14.44,13.73L14.71,14H15.5L20.5,19L19,20.5L14,15.5V14.71L13.73,14.44C12.59,15.41 11.11,16 9.5,16A6.5,6.5 0 0,1 3,9.5A6.5,6.5 0 0,1 9.5,3M9.5,5C7,5 5,7 5,9.5C5,12 7,14 9.5,14C12,14 14,12 14,9.5C14,7 12,5 9.5,5Z"
+            />
+          </svg>
+        </button>
+      </div>
 
-        <div className="accounts-table">
-          <table>
-            <thead>
-              <tr>
-                <th>Account Number</th>
-                <th>Full Name</th>
-                <th>Account Type</th>
-                <th>Initial Balance</th>
-                <th>Email</th>
-                <th>Edit</th>
-              </tr>
-            </thead>
-            <tbody>
-              {filteredData.map((user, index) => (
-                <tr key={index}>
-                  <td>{user.accountNumber}</td>
-                  <td>
-                    {editingIndex === index ? (
-                      <input
-                        className="input-accounts"
-                        type="text"
-                        value={editedUser.fullName}
-                        onChange={(e) =>
-                          handleEditInputChange("fullName", e.target.value)
-                        }
-                      />
-                    ) : (
-                      user.fullName
-                    )}
-                  </td>
-                  <td>
-                    {editingIndex === index ? (
-                      <input
-                        className="input-accounts"
-                        type="text"
-                        value={editedUser.accountType}
-                        onChange={(e) =>
-                          handleEditInputChange("accountType", e.target.value)
-                        }
-                      />
-                    ) : (
-                      user.accountType
-                    )}
-                  </td>
-                  <td>
-                    {editingIndex === index ? (
-                      <input
-                        className="input-accounts"
-                        type="number"
-                        value={editedUser.balance}
-                        onChange={(e) =>
-                          handleEditInputChange("balance", e.target.value)
-                        }
-                      />
-                    ) : (
-                      user.balance
-                    )}
-                  </td>
-                  <td>
-                    {editingIndex === index ? (
-                      <input
-                        className="input-accounts"
-                        type="text"
-                        value={editedUser.email}
-                        onChange={(e) =>
-                          handleEditInputChange("email", e.target.value)
-                        }
-                      />
-                    ) : (
-                      user.email
-                    )}
-                  </td>
-                  <td>
-                    {editingIndex === index ? (
-                      <>
-                        <Edit
-                          onClick={() => handleUpdateUser(index)}
-                          className="edit-icon"
-                        />
-                        <Ban onClick={handleCancelEdit} />
-                      </>
-                    ) : (
+      <div className="accounts-table">
+        <table>
+          <thead>
+            <tr>
+              <th>Account Number</th>
+              <th>Full Name</th>
+              <th>Account Type</th>
+              <th>Balance</th>
+              <th>Email</th>
+              <th>Edit</th>
+            </tr>
+          </thead>
+          <tbody>
+            {filteredData.map((user, index) => (
+              <tr key={index}>
+                <td>{user.accountNumber}</td>
+                <td>
+                  {editingIndex === index ? (
+                    <input
+                      className="input-accounts"
+                      type="text"
+                      value={editedUser.fullName}
+                      onChange={(e) =>
+                        handleEditInputChange("fullName", e.target.value)
+                      }
+                    />
+                  ) : (
+                    user.fullName
+                  )}
+                </td>
+                <td>
+                  {editingIndex === index ? (
+                    <input
+                      className="input-accounts"
+                      type="text"
+                      value={editedUser.accountType}
+                      onChange={(e) =>
+                        handleEditInputChange("accountType", e.target.value)
+                      }
+                    />
+                  ) : (
+                    user.accountType
+                  )}
+                </td>
+                <td>
+                  {editingIndex === index ? (
+                    <input
+                      className="input-accounts"
+                      type="number"
+                      value={editedUser.balance}
+                      onChange={(e) =>
+                        handleEditInputChange("balance", e.target.value)
+                      }
+                    />
+                  ) : (
+                    user.balance
+                  )}
+                </td>
+                <td>
+                  {editingIndex === index ? (
+                    <input
+                      className="input-accounts"
+                      type="text"
+                      value={editedUser.email}
+                      onChange={(e) =>
+                        handleEditInputChange("email", e.target.value)
+                      }
+                    />
+                  ) : (
+                    user.email
+                  )}
+                </td>
+                <td>
+                  {editingIndex === index ? (
+                    <>
                       <Edit
-                        onClick={() => handleEditUser(index)}
+                        onClick={() => handleUpdateUser(index)}
                         className="edit-icon"
                       />
-                    )}
-                  </td>
-                  <td>
-                    <UserX2 onClick={() => handleDeleteUser(index)} />
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
+                      <Ban onClick={handleCancelEdit} />
+                    </>
+                  ) : (
+                    <Edit
+                      onClick={() => handleEditUser(index)}
+                      className="edit-icon"
+                    />
+                  )}
+                </td>
+                <td>
+                  <UserX2
+                    onClick={() => handleDeleteUser(index)}
+                    className="edit-icon"
+                  />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
+    </div>
   );
 }
