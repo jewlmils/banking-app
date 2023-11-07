1a4b64d1f683620195509ce57cc73039f70c2954
jujyne <egmisa.am@gmail.com>
2023-11-07 20:19:02 +0800

    fix buyload logic

--- src/pages/buy-load/BuyLoad.jsx
+++ src/pages/buy-load/BuyLoad.jsx
@@ -5,11 +5,36 @@ import { Header } from "../../components/Header";
 export function BuyLoad() {
   const [recipient, setRecipient] = useState("");
   const [amount, setAmount] = useState("");
+  const [transactionStatus, setTransactionStatus] = useState("");
 
   const handlePurchase = () => {
-    // Implement your purchase logic here
-    console.log("Purchase initiated:", recipient, amount);
+    const currentUser = JSON.parse(localStorage.getItem("currentUser"));
+    const userData = JSON.parse(localStorage.getItem("userData"));
+
+    // Find the user in userData
+    const user = userData.find((user) => user.email === currentUser.email);
+
+    // Ensure recipient is the same as the current user
+    if (amount > 0 && amount <= currentUser.balance) {
+      // Deduct the amount from both currentUser and userData
+      currentUser.balance -= amount;
+      user.balance -= amount;
+
+      // Save the updated currentUser and userData back to localStorage
+      localStorage.setItem("currentUser", JSON.stringify(currentUser));
+      localStorage.setItem("userData", JSON.stringify(userData));
+
+      setTransactionStatus("Transaction successful");
+    } else {
+      setTransactionStatus("Insufficient balance or invalid amount");
+    }
+
+    // Clear the input fields after a transaction attempt
+    setRecipient("");
+    setAmount("");
   };
+
+  const isPurchaseButtonDisabled = recipient.trim() === "" || amount <= 0;
 
   return (
     <div className="container">
@@ -18,9 +43,13 @@ export function BuyLoad() {
       <div className="input-section">
         <label htmlFor="recipient">Recipient</label>
         <input
-          type="text"
+          type="number"
           id="recipient"
-          placeholder="Enter recipient name"
+          placeholder="Enter phone number"
+          in="0"
+          max="99999999999"
+          pattern="[0-9]{11}"
+          required
           value={recipient}
           onChange={(e) => setRecipient(e.target.value)}
         />
@@ -35,9 +64,14 @@ export function BuyLoad() {
           onChange={(e) => setAmount(e.target.value)}
         />
       </div>
-      <button className="purchase-btn" onClick={handlePurchase}>
+      <button
+        className="purchase-btn"
+        onClick={handlePurchase}
+        disabled={isPurchaseButtonDisabled}
+      >
         Purchase Load
       </button>
+      {transactionStatus && <p>{transactionStatus}</p>}
     </div>
   );
 }
