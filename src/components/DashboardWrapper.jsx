f4bf760af2f5b9f01229996835ea983c2e5f537d
jujyne <egmisa.am@gmail.com>
2023-11-09 16:43:55 +0800

    Add: responsiveness on sidebar and header

--- src/components/DashboardWrapper.jsx
+++ src/components/DashboardWrapper.jsx
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Sidebar } from "./sidebar/Sidebar";
 import { Header } from "./Header";
 import { Outlet } from "react-router";
 
 export function DashboardWrapper() {
+  const [showSidebar, setShowSidebar] = useState(true);
+
+  const toggleSidebar = () => {
+    setShowSidebar(!showSidebar);
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      setShowSidebar(window.innerWidth > 851);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    // Initial check
+    handleResize();
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <div className="body">
-      <Header />
+      <Header toggleSidebar={toggleSidebar} />
       <main>
-        <Sidebar />
+        {showSidebar && <Sidebar />}
         <section className="content">
           <div className="content-container">
             <Outlet />
--- src/components/PrivateRoutes.jsx
+++ src/components/PrivateRoutes.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { Navigate, Outlet, useNavigate } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 
 export const PrivateRoutes = () => {
   const navigate = useNavigate();
