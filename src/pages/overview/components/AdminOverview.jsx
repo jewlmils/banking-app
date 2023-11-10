d831e22139c8073d2e739c79edc556c5a7a9be4c
jujyne <egmisa.am@gmail.com>
2023-11-10 19:28:23 +0800

    add: dynamic information on AdminOverview

--- src/pages/overview/components/AdminOverview.jsx
+++ src/pages/overview/components/AdminOverview.jsx
@@ -1,31 +1,43 @@
-import { userData } from "../../../Data";
+import React, { useState, useEffect } from "react";
 import { Currency } from "../../currency/Currency";
 
-const totalBalance = userData
-  .reduce((total, user) => total + parseFloat(user.balance), 0)
-  .toFixed(2);
-const nonAdminUsers = userData.filter((user) => !user.isAdmin);
-
-const numberOfClients = nonAdminUsers.length;
-
 export function AdminOverview() {
+  const [totalBalance, setTotalBalance] = useState(0);
+  const [numberOfClients, setNumberOfClients] = useState(0);
   const user = JSON.parse(localStorage.getItem("currentUser"));
+  const userData = JSON.parse(localStorage.getItem("userData"));
+
+  useEffect(() => {
+    const nonAdminUsers = userData.filter((userData) => !userData.isAdmin);
+
+    const calculatedTotalBalance = nonAdminUsers
+      .reduce((total, userData) => total + parseFloat(userData.balance), 0)
+      .toFixed(2);
+
+    setTotalBalance(calculatedTotalBalance);
+    setNumberOfClients(nonAdminUsers.length);
+  }, [user.balance, (JSON.parse(localStorage.getItem("userData")))]); // Run the effect whenever user.balance or userData changes
+
+  const formattedTotalBalance = new Intl.NumberFormat("en-PH", {
+    style: "currency",
+    currency: "PHP",
+  }).format(totalBalance);
+
   return (
     <div className="overview-admin">
       <div className="stats-container">
-      <div className="deposit-container">
-        <h1>Total Deposits</h1>
-        <h2>₱{totalBalance}</h2>
+        <div className="deposit-container">
+          <h1>Total Deposits</h1>
+          <h2>{formattedTotalBalance}</h2>
+        </div>
+
+        <div className="clients-container">
+          <h1>Number of Clients</h1>
+          <h2>{numberOfClients}</h2>
+        </div>
       </div>
 
-      <div className="clients-container">
-        <h1>Number of Clients</h1>
-        <h2>{numberOfClients}</h2>
-      </div>
-      </div>
-      
-      <Currency/>
+      <Currency />
     </div>
-    
   );
 }
--- src/pages/overview/components/CustomerOverview.jsx
+++ src/pages/overview/components/CustomerOverview.jsx
@@ -2,36 +2,45 @@ import { useState, useEffect } from "react";
 import { Currency } from "../../currency/Currency";
 
 export function CustomerOverview() {
-    const user = JSON.parse(localStorage.getItem("currentUser"));
-    const [balanceDisplay, setbalanceDisplay] = useState();
-    const [nameDisplay, setNameDisplay] = useState();
-    const [accountNumberDisplay, setAccountNumberDisplay] =useState()
-    useEffect(() => {
-      setbalanceDisplay(user.balance);
-    }, [user.balance]);
-  
-    useEffect(() => {
-      setNameDisplay(user.fullName);
-    }, [user.fullName]);
+  const user = JSON.parse(localStorage.getItem("currentUser"));
+  const [getBalance, setGetBalance] = useState(0);
+  const [nameDisplay, setNameDisplay] = useState(0);
+  const [accountNumberDisplay, setAccountNumberDisplay] = useState(0);
+  const [accountType, setAccountType] = useState(0);
 
-    useEffect(() => {
-      setAccountNumberDisplay(user.accountNumber);
-    }, [user.accountNumber]);
-  
-    return (
-      <div className="overview-customer">
-        <div className="card-container">
-          
-          <span className="card-account-number">{accountNumberDisplay}</span>
-          <span className="card-balance">₱{balanceDisplay}</span>
-          <span className="card-name">{nameDisplay}</span>
-          <span className="card-expiration">10/28</span>
-          <span className="account-type">Savings Account</span>
-        </div>
-        <div className="currency-container">
-            <Currency/>
-        </div>
+  useEffect(() => {
+    setGetBalance(user.balance);
+  }, [user.balance]);
+
+  useEffect(() => {
+    setNameDisplay(user.fullName);
+  }, [user.fullName]);
+
+  useEffect(() => {
+    setAccountNumberDisplay(user.accountNumber);
+  }, [user.accountNumber]);
+
+  useEffect(() => {
+    setAccountType(user.accountType);
+  }, [user.accountType]);
+
+  const formattedBalance = new Intl.NumberFormat("en-PH", {
+    style: "currency",
+    currency: "PHP",
+  }).format(getBalance);
+
+  return (
+    <div className="overview-customer">
+      <div className="card-container">
+        <span className="card-account-number">{accountNumberDisplay}</span>
+        <span className="card-balance">{formattedBalance}</span>
+        <span className="card-name">{nameDisplay}</span>
+        <span className="card-expiration">10/28</span>
+        <span className="account-type">{accountType}</span>
       </div>
-    );
-  }
-  
+      <div className="currency-container">
+        <Currency />
+      </div>
+    </div>
+  );
+}
